dca84418034daf91291fa7c5aad8cd106f417d2c
Lucasdfg <cervantes.lucas99@gmail.com>
2019-10-29 17:57:48 -0600

    added fist test

---
+++ src/Components/Demo.js
@@ -0,0 +1,56 @@
+import React, {Component} from "react"
+
+class Demo extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        bgColor: "",
+        randomItem: ""
+      }
+    }
+  
+    randomItemGenerator = () => (
+        this.myArray[Math.floor(Math.random()*this.myArray.length)]
+    )
+    componentDidMount(){
+        this.setState({randomItem:this.randomItemGenerator()})
+    }
+    
+    boxClick = (e) => {
+      this.setState({
+        bgColor: this.state.randomItem
+      })
+    }
+    resetForm = () => {
+        this.setState({
+        
+            randomItem: ""
+        })
+     }
+    myArray = [
+        "red",
+        "green"
+    ];
+  
+    render() {
+      return (
+        <div className="App">
+  
+  
+          <article className='experimentsHolder'>
+            <h2>Test 3</h2>
+            <p>This is an example of an onClick event by react.</p>
+            <div className="boxClickCss" 
+            style={{backgroundColor: this.state.bgColor}}
+             onClick={this.boxClick}>Click Me!</div>
+          </article>
+          <div><h3>{this.state.randomItem}</h3></div>
+                <div className="shift" onClick={this.boxClick}>red</div>
+                <div className="shift" style={{backgroundColor: this.state.bgColor}} onClick={this.boxClick, this.resetForm}>red</div>
+  
+        </div>
+      );
+    }
+  }
+  export default Demo;
---
+++ src/Components/Table.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Table extends React.Component{
+    state = {
+        
+    }
+    render(){
+        return(
+            <div>
+                <div className="shift">red</div>
+                <div className="shift">red</div>
+            </div>
+        )
+    }
+}
+export default Table;
