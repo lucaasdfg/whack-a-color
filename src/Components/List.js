adf0ac5213cdc83d56095f94c405496776099f47
Lucasdfg <cervantes.lucas99@gmail.com>
2019-11-04 07:55:39 -0600

    passed props

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,22 +1,16 @@
 import React from "react";
 
-class List extends React.Component{
-    render(){
-        const {list} = this.props;
-        const numbersList = list.map(num => {
-            return(
-                <div className="list" key={num.id}>
-                    <div>Name: {num.name}</div>
-                    <div>Age: {num.age}</div>
-                    <div>Belt: {num.belt}</div>
-                </div>
-            )
-        })
-        return(
-            <div className="num-list">
-            {numbersList}
-            </div>
-        )
-    }
-}
+const List = props => {
+  const { list } = props;
+  const numbersList = list.map(num => {
+    return (
+      <div className="list" key={num.id}>
+        <div>Name: {num.name}</div>
+        <div>Age: {num.age}</div>
+        <div>Belt: {num.belt}</div>
+      </div>
+    );
+  });
+  return <div className="num-list">{numbersList}</div>;
+};
 export default List;
